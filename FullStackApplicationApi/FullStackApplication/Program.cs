4cc33f8ace81d931695249a5daec22b06ba35a1b
Kavit Trivedi <kavittrivedi@hotmail.com>
2024-07-07 10:55:05 +0530

    Code cleanup.

--- FullStackApplicationApi/FullStackApplication/Program.cs
+++ FullStackApplicationApi/FullStackApplication/Program.cs
@@ -2,46 +2,6 @@ using FullStackApplication.Services;
 using FullStackApplication.Services.Contract;
 using FullStackApplication.Services.Implementation;
 
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-//builder.Services.AddControllersWithViews();
-
-//// Register configuration settings
-//builder.Services.Configure<ApiSettings>(builder.Configuration.GetSection("ApiSettings"));
-
-//// Retrieve API settings to configure HttpClient
-//var apiSettings = builder.Configuration.GetSection("ApiSettings").Get<ApiSettings>();
-
-//var httpClientHandler = new HttpClientHandler();
-//httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
-
-//builder.Services.AddHttpClient<ApiService>(client =>
-//{
-//    client.BaseAddress = new Uri(apiSettings.BaseUrl);
-//}).ConfigurePrimaryHttpMessageHandler(() => httpClientHandler);
-
-//builder.Services.AddScoped<ICategoryService, CategoryService>();
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//}
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
-
-//app.UseRouting();
-
-//app.UseAuthorization();
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-//app.Run();
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.ClearProviders();
